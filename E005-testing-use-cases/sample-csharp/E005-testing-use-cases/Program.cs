2a6bbbb49a7b70987c403ad06d7c7206888e5249
Kerry Street <@kcstreet>
2012-09-02 12:15:25 -0700

    E005 comments

--- E005-testing-use-cases/sample-csharp/E005-testing-use-cases/Program.cs
+++ E005-testing-use-cases/sample-csharp/E005-testing-use-cases/Program.cs
@@ -18,12 +18,60 @@ namespace E005_testing_use_cases
     // to see this in NUnit using Visual Studio, try right-clicking on the E005-testing-use-cases project
     // and navigate to Test With --> NUnit and click "Run" in the NUnit test runner to see the test output
 
+    // this was the first specification example mentioned in podcast Episode 5
+    // typically one specification class like this is used per aggregate command method
+    // this class grouping of specifications and use cases that test each method is also called a "Test Fixture"
+    // each [Test] specification covering a use case is also called a "Unit Test"
+    // in this case, testing the behavior of the "TransferShipmentToCargoBay" method on the factory aggregate
+    // and the serveral use cases related to that command method
+    public sealed class when_transfer_shipment_to_cargo_bay : factory_specs
+    {
+        // Use Case: Empty Shipment
+        [Test]
+        public void empty_shipment()
+        {
+            // You will notice the use of the "Given, When, Then" structure inside specifications
 
+            // 
+            Given = new IEvent[] { new EmployeeAssignedToFactory("yoda")  };
+
+            // When (we do something with a method, like transfer a shipment to the cargo bay)
+            When = f => f.TransferShipmentToCargoBay("some shipment", new CarPart[0]);
+            ThenException = ex => ex.Message.Contains("Empty shipments are not accepted!");
+        }
+
+        // Use Case: Empty Shipment and No Workers at the Factory
+        [Test]
+        public void empty_shipment_comes_to_empty_factory()
+        {
+            // Instead of some empty array of events like: Given = new IEvent[];
+            // It is just left out, which does the same thing:  No Pre-Existing Events Yet!
+            // So: Given NO EVENTS
+
+            When = f => f.TransferShipmentToCargoBay("some shipment", new[] {new CarPart("chassis", 1)});
+            ThenException = ex => ex.Message.Contains("has to be somebody at factory");
+        }
+        [Test]
+        public void there_already_are_two_shipments()
+        {
+            Given = new IEvent[]
+                {
+                    new EmployeeAssignedToFactory("chubakka"), 
+                    new ShipmentTransferredToCargoBay("shipmt-11", new CarPart("engine",3)), 
+                    new ShipmentTransferredToCargoBay("shipmt-12", new CarPart("wheels", 40)), 
+                };
+            When = f => f.TransferShipmentToCargoBay("shipmt-13", new CarPart("bmw6", 20));
+            ThenException = ex => ex.Message.Contains("More than two shipments can't fit");
+        }
+    }
+
+    // and another test fixture to test the behavior of the "AssignEmployeeToFactory" method
     public sealed class when_assign_employee_to_factory : factory_specs
     {
         [Test]
         public void empty_factory()
         {
+            // Given no events
             When = f => f.AssignEmployeeToFactory("fry");
             Then = new IEvent[]
                 {
@@ -45,35 +93,6 @@ namespace E005_testing_use_cases
         {
             When = f => f.AssignEmployeeToFactory("bender");
             ThenException = ex => ex.Message.Contains("Guys with name 'bender' are trouble");
-        }
-    }
-
-    public sealed class when_transfer_shipment_to_cargo_bay : factory_specs
-    {
-        [Test]
-        public void empty_shipment()
-        {
-            Given = new IEvent[] { new EmployeeAssignedToFactory("yoda")  };
-            When = f => f.TransferShipmentToCargoBay("some shipment", new CarPart[0]);
-            ThenException = ex => ex.Message.Contains("Empty shipments are not accepted!");
-        }
-        [Test]
-        public void empty_shipment_comes_to_empty_factory()
-        {
-            When = f => f.TransferShipmentToCargoBay("some shipment", new[] {new CarPart("chassis", 1)});
-            ThenException = ex => ex.Message.Contains("has to be somebody at factory");
-        }
-        [Test]
-        public void there_already_are_two_shipments()
-        {
-            Given = new IEvent[]
-                {
-                    new EmployeeAssignedToFactory("chubakka"), 
-                    new ShipmentTransferredToCargoBay("shipmt-11", new CarPart("engine",3)), 
-                    new ShipmentTransferredToCargoBay("shipmt-12", new CarPart("wheels", 40)), 
-                };
-            When = f => f.TransferShipmentToCargoBay("shipmt-13", new CarPart("bmw6", 20));
-            ThenException = ex => ex.Message.Contains("More than two shipments can't fit");
         }
     }
 
