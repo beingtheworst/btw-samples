f10a1b9aa6fdff8c49def0274c2ab69b0a6b1a79
Rinat Abdullin <rinat.abdullin@gmail.com>
2012-12-02 17:08:15 +0600

    Polishing value objects sample

--- E016-value-objects/sample-csharp/Program.cs
+++ E016-value-objects/sample-csharp/Program.cs
@@ -7,14 +7,32 @@ namespace E016
     {
         public static void Main()
         {
-            Console.WriteLine(10m.Eur() + 15m.Eur());
+            PrintOperation("10m.Eur() + 15m.Eur()",() => 10m.Eur() + 15m.Eur());
+            PrintOperation("10m.Rub() + 15m.Eur()", () => 10m.Rub() + 15m.Eur());
 
-            Console.WriteLine(32m.Rub() > 30m.Rub());
+            PrintOperation("32m.Rub() > 30m.Rub()", () => 32m.Rub() > 30m.Rub());
+            PrintOperation("10m.Eur() / 2m.Eur()", () => 10m.Eur() / 2m.Eur());
 
-            Console.Read();
+            PrintOperation("32m.Rub() > 2m.Eur()", () => 32m.Rub() > 2m.Eur());
+
+            Console.ReadLine();
 
 
+        }
 
+        static void PrintOperation<T>(string description, Func<T> operation)
+        {
+            Console.WriteLine("Evaluating: {0}", description);
+
+            try
+            {
+                Console.WriteLine("  Result: " + operation());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("  Error:  " + ex.Message);
+            }
+            Console.WriteLine();
         }
     }
 }
