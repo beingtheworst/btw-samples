cb29916977f8d84083039f20d83f00a351a72278
Kerry Street <@kcstreet>
2012-12-05 13:19:42 -0800

    E17 comments

--- E017-view-projections/sample-csharp/E017.Domain/Projections/WorkerRegistryProjection.cs
+++ E017-view-projections/sample-csharp/E017.Domain/Projections/WorkerRegistryProjection.cs
@@ -9,6 +9,10 @@ namespace E017.Projections
     /// </summary>
     public sealed class WorkerRegistryProjection
     {
+        // using Tuple class shortcut here for this simple sample to avoid creating a Value Object
+        // TUple is just a quick way to define a bag of stuff with specific property types.
+        // TODO: Replace Tuple with Value Object that has EmployeeName and his/her Factory Id Association.
+
         public List<Tuple<string, FactoryId>> List = new List<Tuple<string, FactoryId>>();
   
         public void When(EmployeeAssignedToFactory e)
